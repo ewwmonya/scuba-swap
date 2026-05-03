476c6d04c5f17c9ea29b4635da57715abb281757
ewwmonya <savvymallen85@gmail.com>
2026-05-02 21:13:54 -0400

    added the ava link to the public layout

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,5 +1,6 @@
 import Container from '@/components/globals/Container';
 import NavbarServer from '@/components/navbar/NavbarServer';
+import Script from 'next/script';
 import { ReactNode } from 'react';
 
 export default async function PublicLayout({
@@ -11,6 +12,10 @@ export default async function PublicLayout({
 		<>
 			<NavbarServer />
 			<Container>{children}</Container>
+			<Script
+				strategy='beforeInteractive'
+				src='https://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=5b1d16afa62496d744376c3343f4c41d41a4dba9'
+			/>
 		</>
 	);
 }
