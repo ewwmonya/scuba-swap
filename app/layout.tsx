667ad77da8de8d66c73d30793b4f848c175d2f66
ewwmonya <savvymallen85@gmail.com>
2026-05-02 20:51:20 -0400

    trial 4 for the aff

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,12 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en'>
+			<head>
+				<Script
+					strategy='beforeInteractive'
+					src='https://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=5b1d16afa62496d744376c3343f4c41d41a4dba9'
+				/>
+			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased  bg-gray-100`}
 			>
@@ -37,10 +43,6 @@ export default function RootLayout({
 					<Suspense fallback={'Loading...'}>{children}</Suspense>
 				</main>
 				<Footer />
-				<Script
-					strategy='afterInteractive'
-					src='https://classic.avantlink.com/affiliate_app_confirm.php?mode=js&authResponse=5b1d16afa62496d744376c3343f4c41d41a4dba9'
-				/>
 			</body>
 		</html>
 	);
